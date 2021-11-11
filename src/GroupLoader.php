1756beb5d47f9361ba143b0e9817aa760a0059a8
Tomasz Rusiecki <rusiecki.tom@gmail.com>
2021-11-11 12:05:22 +0100

    group loader

---
+++ src/GroupLoader.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Devr\Loader;
+
+final class GroupLoader
+{
+    private $loaders = [];
+
+    public function run(): void
+    {
+        foreach ($this->loaders as $loader) {
+            $loader->run();
+        }
+    }
+
+    /**
+     * @param LoaderInterface $loader
+     */
+    public function add(LoaderInterface $loader): void
+    {
+        $this->loaders[] = $loader;
+    }
+}
